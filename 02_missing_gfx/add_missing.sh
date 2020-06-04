133cc25c09bc8f77ceffd76ff0fd0c0a73dd53fa
Michael Hitchens <mdhitchens@gmail.com>
2020-06-04 19:27:41 -0400

    add_missing.sh: script to include missing files into MPQ
    uses mpqadd

---
+++ 02_missing_gfx/add_missing.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eux
+
+if [ $# -ne 1 ]
+then
+	echo Usage: $0 file.mpq
+	exit 1
+fi
+
+mpqadd="../!Tools/mpqadd/build/mpqadd"
+
+find levels/ -type f | "${mpqadd}" "$1"
+find monsters/ -type f | "${mpqadd}" "$1"
+find plrgfx/ -type f | "${mpqadd}" "$1"
