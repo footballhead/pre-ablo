2a3e0cd699b3e0a2bab2693899ceac73b47e85d7
Michael Hitchens <mdhitchens@gmail.com>
2020-06-18 23:27:26 -0400

    Override version from dplay.dll instead
    I need to think more about this approach and how it integrates with the launcher. Currently, I can't turn this off (without using a different dplay.dll)

--- 00_dplay/dll.cpp
+++ 00_dplay/dll.cpp
@@ -1,6 +1,12 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
 
+#include "version.h"
+
+namespace {
+char* version = reinterpret_cast<char*>(0x004AA6F4);
+} // namespace
+
 extern "C"
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -10,6 +16,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        strncpy(version, PRDE_VERSION, 17);
+        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
