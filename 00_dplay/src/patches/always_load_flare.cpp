d8b8db5d8fa7ed79bd770760b946fa0747621a58
Michael Hitchens <mdhitchens@gmail.com>
2020-06-21 15:27:21 -0400

    dplay always_load_flare: extend to other missiles
    There is still more work to be done to decrease loading times

--- 00_dplay/src/patches/always_load_flare.cpp
+++ 00_dplay/src/patches/always_load_flare.cpp
@@ -8,10 +8,18 @@ namespace {
 // Imports
 //
 
-auto pFlareCel = reinterpret_cast<BYTE**>(0x005F71E0);
-auto pFlarexpCel = reinterpret_cast<BYTE**>(0x005FC89C);
+auto** const pMagballCels = reinterpret_cast<BYTE**>(0x005F6F90); // array of pointers
+auto** const pThinLghningCel = reinterpret_cast<BYTE**>(0x005FC0A0); // pointer of pointers
+auto** const pFlareCel = reinterpret_cast<BYTE**>(0x005F71E0); // pointer to pointer
+auto** const pFlarexpCel = reinterpret_cast<BYTE**>(0x005FC89C); // pointer to pointer
 
 auto LoadFileInMem = reinterpret_cast<BYTE* (__fastcall *)(const char* pszName)>(0x00482790);
+
+//
+// Locals
+//
+
+bool loaded = false;
 
 //
 // Hooks
@@ -19,40 +27,66 @@ auto LoadFileInMem = reinterpret_cast<BYTE* (__fastcall *)(const char* pszName)>
 
 void load_flare_gfx()
 {
-    if (*pFlareCel) {
-        // Already loaded
+    if (loaded) {
         return;
     }
+
     // Could also reuse the string literals but meh this is more readable
+    char buffer[256]{'\0'};
+    for (auto i = 1; i <= 8; ++i) {
+        snprintf(buffer, 256, "Monsters\\Magma\\Magball%i.CEL", i);
+        pMagballCels[i] = LoadFileInMem(buffer);
+    }
+    *pThinLghningCel = LoadFileInMem("Monsters\\Thin\\Lghning.CEL");
     *pFlareCel = LoadFileInMem("Monsters\\Succ\\Flare.CEL");
     *pFlarexpCel = LoadFileInMem("Monsters\\Succ\\Flarexp.CEL");
+
+    loaded = true;
 }
 
 void free_flare_gfx()
 {
-    if (*pFlareCel == nullptr) {
-        // Already freed
+    if (!loaded) {
         return;
     }
+
+    for (auto i = 0; i < 8; ++i) {
+        GlobalUnlock(GlobalHandle(pMagballCels[i]));
+        GlobalFree(GlobalHandle(pMagballCels[i]));
+        pMagballCels[i] = nullptr;
+    }
+    GlobalUnlock(GlobalHandle(*pThinLghningCel));
+    GlobalFree(GlobalHandle(*pThinLghningCel));
+    *pThinLghningCel = nullptr;
     GlobalUnlock(GlobalHandle(*pFlareCel));
     GlobalFree(GlobalHandle(*pFlareCel));
     *pFlareCel = nullptr;
     GlobalUnlock(GlobalHandle(*pFlarexpCel));
     GlobalFree(GlobalHandle(*pFlarexpCel));
     *pFlarexpCel = nullptr;
+
+    loaded = false;
 }
 
 } // namespace
 
 void always_load_flare_main()
 {
-    // Hook our load function (note that this is in a for loop so the hooked fn could be called many times)
-    patch_call(0x00401969, (void*)load_flare_gfx);
-    // Nop the code that already exists
-    nop(0x00401969 + 5, 0x004019A1);
+    //
+    // Load
+    //
 
-    // Hook our free function (same for loop note applies)
-    patch_call(0x0040B807, (void*)free_flare_gfx);
     // Nop the code that already exists
-    nop(0x0040B807 + 5, 0x0040B867);
+    nop(0x004018CB, 0x004019A1);
+    // Hook our load function (note that this is in a for loop so the hooked fn could be called many times)
+    patch_call(0x004018CB, (void*)load_flare_gfx);
+
+    //
+    // Free
+    //
+
+    // Nop the code that already exists
+    nop(0x0040B76B, 0x0040B867);
+    // Hook our free function (same for loop note applies)
+    patch_call(0x0040B76B, (void*)free_flare_gfx);
 }
