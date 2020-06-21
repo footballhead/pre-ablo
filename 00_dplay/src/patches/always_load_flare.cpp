b54f398306de9216506abe72538a1f6fc93cffef
Michael Hitchens <mdhitchens@gmail.com>
2020-06-21 17:42:09 -0400

    dplay always_load_flare: make more specific
    keep the mtype checks but add some double-load/double free checks

--- 00_dplay/src/patches/always_load_flare.cpp
+++ 00_dplay/src/patches/always_load_flare.cpp
@@ -19,74 +19,118 @@ auto LoadFileInMem = reinterpret_cast<BYTE* (__fastcall *)(const char* pszName)>
 // Locals
 //
 
-bool loaded = false;
+bool magball_loaded = false;
 
 //
 // Hooks
 //
 
-void load_flare_gfx()
+void load_magball()
 {
-    if (loaded) {
+    // The real reason we're here: early return if already loaded
+    if (magball_loaded) {
         return;
     }
-
-    // Could also reuse the string literals but meh this is more readable
+    magball_loaded = true;
+    // Displaced code
     char buffer[256]{'\0'};
-    for (auto i = 1; i <= 8; ++i) {
-        snprintf(buffer, 256, "Monsters\\Magma\\Magball%i.CEL", i);
+    for (auto i = 0; i < 8; ++i) {
+        snprintf(buffer, 256, "Monsters\\Magma\\Magball%i.CEL", i + 1);
         pMagballCels[i] = LoadFileInMem(buffer);
     }
-    *pThinLghningCel = LoadFileInMem("Monsters\\Thin\\Lghning.CEL");
-    *pFlareCel = LoadFileInMem("Monsters\\Succ\\Flare.CEL");
-    *pFlarexpCel = LoadFileInMem("Monsters\\Succ\\Flarexp.CEL");
-
-    loaded = true;
 }
 
-void free_flare_gfx()
+void free_magball()
 {
-    if (!loaded) {
+    // The real reason we're here: early return if already freed
+    if (!magball_loaded) {
         return;
     }
-
+    magball_loaded = false;
+    // displaced code
     for (auto i = 0; i < 8; ++i) {
         GlobalUnlock(GlobalHandle(pMagballCels[i]));
         GlobalFree(GlobalHandle(pMagballCels[i]));
         pMagballCels[i] = nullptr;
     }
+}
+
+void load_thin_lightning()
+{
+    // The real reason we're here: early return if already loaded
+    if (*pThinLghningCel) {
+        return;
+    }
+    // displaced code
+    *pThinLghningCel = LoadFileInMem("Monsters\\Thin\\Lghning.CEL");
+}
+
+void free_thin_lightning()
+{
+    // The real reason we're here: early return if already freed
+    if (!*pThinLghningCel) {
+        return;
+    }
+    // displaced code
     GlobalUnlock(GlobalHandle(*pThinLghningCel));
     GlobalFree(GlobalHandle(*pThinLghningCel));
     *pThinLghningCel = nullptr;
+}
+
+void load_flare_gfx()
+{
+    // The real reason we're here: early return if already loaded
+    if (*pFlareCel) {
+        return;
+    }
+    // displaced code
+    *pFlareCel = LoadFileInMem("Monsters\\Succ\\Flare.CEL");
+    *pFlarexpCel = LoadFileInMem("Monsters\\Succ\\Flarexp.CEL");
+}
+
+void free_flare_gfx()
+{
+    // The real reason we're here: early return if already freed
+    if (!*pFlareCel) {
+        return;
+    }
+    // displaced code
     GlobalUnlock(GlobalHandle(*pFlareCel));
     GlobalFree(GlobalHandle(*pFlareCel));
     *pFlareCel = nullptr;
     GlobalUnlock(GlobalHandle(*pFlarexpCel));
     GlobalFree(GlobalHandle(*pFlarexpCel));
     *pFlarexpCel = nullptr;
-
-    loaded = false;
 }
 
 } // namespace
 
 void always_load_flare_main()
 {
-    //
-    // Load
-    //
+    // In each case we keep the mtype check but redirect loading/freeing to our own code.
+    // To hook, we first nop the problem code, then we patch in a call to our fixed code.
 
-    // Nop the code that already exists
-    nop(0x004018CB, 0x004019A1);
-    // Hook our load function (note that this is in a for loop so the hooked fn could be called many times)
-    patch_call(0x004018CB, (void*)load_flare_gfx);
+    // Load magball
+    nop(0x004018F4, 0x00401940);
+    patch_call(0x004018FA, (void*)load_magball);
 
-    //
-    // Free
-    //
+    // Free magball
+    nop(0x0040B78B, 0x0040B7CD);
+    patch_call(0x0040B78E, (void*)free_magball);
 
-    // Nop the code that already exists
-    nop(0x0040B76B, 0x0040B867);
-    // Hook our free function (same for loop note applies)
-    patch_call(0x0040B76B, (void*)free_flare_gfx);
+    // Load thin lightning
+    nop(0x0040195A, 0x00401969);
+    patch_call(0x0040195A, (void*)load_thin_lightning);
+    
+    // Free thin lightning
+    nop(0x0040B7E1, 0x0040B807);
+    patch_call(0x0040B7E1, (void*)free_thin_lightning);
+
+    // load flare
+    nop(0x00401983, 0x004019A1);
+    patch_call(0x00401983, (void*)load_flare_gfx);
+
+    // Free flare
+    nop(0x0040B81B, 0x0040B867);
+    patch_call(0x0040B81B, (void*)free_flare_gfx);
 }
