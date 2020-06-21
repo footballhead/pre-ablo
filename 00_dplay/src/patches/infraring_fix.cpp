3b9c068c523703d940a495dc7d89c0fcd75e2496
Michael Hitchens <mdhitchens@gmail.com>
2020-06-21 13:35:38 -0400

    dplay: infraring_fix

---
+++ 00_dplay/src/patches/infraring_fix.cpp
@@ -0,0 +1,34 @@
+#include "patches.hpp"
+
+#include "util.hpp"
+
+namespace {
+
+//
+// Imports
+//
+
+auto const DrawUniqueInfo = reinterpret_cast<void (__fastcall *)()>(0x00423BA4);
+// curruitem is an item struct but we only care about this one address
+auto* const curruitem_iUid = reinterpret_cast<int8_t* const>(0x005DED6F);
+
+//
+// Hooks
+//
+
+void __fastcall DrawUniqeInfo_Wrapper()
+{
+    if (*curruitem_iUid != -1) {
+        DrawUniqueInfo();
+    }
+}
+
+} // namespace
+
+void infraring_fix_main()
+{
+    bool ok = true;
+    ok &= patch_call(0x00448C2B, (void*)DrawUniqeInfo_Wrapper);
+    ok &= patch_call(0x004812F9, (void*)DrawUniqeInfo_Wrapper);
+    printf("%s %s\n", __func__, ok ? "success" : "fail");
+}
