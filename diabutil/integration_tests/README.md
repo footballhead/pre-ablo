ea261fba402d2e450426678f3cf4d3167d837c4d
Michael Hitchens <mdhitchens@gmail.com>
2022-02-27 11:48:01 -0500

    diabutil: replace setup_assets.sh

--- diabutil/integration_tests/README.md
+++ diabutil/integration_tests/README.md
@@ -6,7 +6,7 @@ WARNING: This is a work-in-progress!
 
 * `pip install --user pipenv`
 * `pipenv install`
-* `./setup_assets.sh <prdemo.mpq> <retail.mpq>`
+* `pipenv run python setup_assets.py <prdemo.mpq> <retail.mpq>`
     * You can get the pre-release demo MPQ from diablo evolution
     * You can buy Diablo on GoG for retail MPQ
     * Look I'm already morally conflicted about distributing files in missing_gfx OK just buy the game
@@ -19,4 +19,6 @@ We do file comparisons when we can, although you can inpsect files in output/ fo
 
 ## Developer commands
 
+* `pipenv install -d`
 * `pipenv run yapf -i run_integration_tests.py`
+* `pipenv run yapf -i setup_assets.py`
---
+++ diabutil/integration_tests/setup_assets.py
@@ -0,0 +1,52 @@
+'''
+Gather assets for integration tests
+'''
+
+# Please run yapf on me!
+
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+# TODO: make BUILD_DIR configurable or smarter
+BUILD_DIR = (Path('..') / 'build').resolve()
+ASSETS_DIR = Path('assets').resolve()
+
+# TODO: This is different on Windows vs Linux
+mpqextract = BUILD_DIR / 'mpqextract' / 'Debug' / 'mpqextract'
+
+# Fed as input to subprocess so must be byte-string
+PRDEMO_LISTFILE = b'''data/bigtgold.cel
+gendata/mainmenu.pal
+data/inv/objcurs.cel
+monsters/bat/bata.cel
+objects/vapor1.cel
+'''
+
+# Fed as input to subprocess so must be byte-string
+RETAIL_LISTFILE = b'''ui_art/font42.bin
+ui_art/font42g.pcx
+levels/l3data/l3.amp
+levels/l3data/l3.cel
+levels/l3data/l3.min
+'''
+
+
+def main():
+    if len(sys.argv) < 3:
+        print(f'Usage: {sys.argv[0]} prdemo.mpq retail.mpq')
+        exit(1)
+
+    # We chdir so all paths must be absolute
+    prdemo_mpq = Path(sys.argv[1]).resolve()
+    retail_mpq = Path(sys.argv[2]).resolve()
+
+    ASSETS_DIR.mkdir(parents=True, exist_ok=True)
+    os.chdir(ASSETS_DIR)
+    subprocess.run([mpqextract, prdemo_mpq], input=PRDEMO_LISTFILE, check=True)
+    subprocess.run([mpqextract, retail_mpq], input=RETAIL_LISTFILE, check=True)
+
+
+if __name__ == '__main__':
+    main()
